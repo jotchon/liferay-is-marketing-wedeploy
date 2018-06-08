adfc7d81968c3ae846dbe85bef8eb525f8741718
ryanschuhler <ryan.schuhler@liferay.com>
2018-06-07 17:29:00 -0700

    Add db upgrade script

---
+++ scripts/update-db.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+CONTAINTER_DIR=/docker-entrypoint-initdb.d
+CONTAINTER_NAME=${2:-web_prod_mariadb}
+DATABASE_URL=$1
+INIT_DB_DIR=../mariadb/docker-entrypoint-initdb.d
+VOLUME_NAME=${3:-liferay-is-marketing-wedeploy_web_prod_mariadb}
+
+docker volume rm ${VOLUME_NAME}
+
+rm ${INIT_DB_DIR}/*
+curl -o ${INIT_DB_DIR}/${DATABASE_URL##*/} -fSL ${DATABASE_URL}
+
+docker-compose up ${CONTAINTER_NAME}
