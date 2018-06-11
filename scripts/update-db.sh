fa9135c4ce75d5ba0ad98a7a7f9e3b3346a6a884
ryanschuhler <ryan.schuhler@liferay.com>
2018-06-11 11:55:50 -0700

    Add Update credentials

--- scripts/update-db.sh
+++ scripts/update-db.sh
@@ -2,13 +2,19 @@
 
 CONTAINTER_DIR=/docker-entrypoint-initdb.d
 CONTAINTER_NAME=${2:-www_prod_mariadb}
+CREDENTIALS=""
 DATABASE_URL=$1
 INIT_DB_DIR=../mariadb/docker-entrypoint-initdb.d
 VOLUME_NAME=${3:-liferay-is-marketing-wedeploy_www_prod_mariadb}
 
+if [ -f ".username" ] && [ -f ".password" ]; then
+	CREDENTIALS="-u $(cat .username):$(cat .password)"
+fi
+
+docker rm ${CONTAINTER_NAME}
 docker volume rm ${VOLUME_NAME}
 
 rm ${INIT_DB_DIR}/*
-curl -o ${INIT_DB_DIR}/${DATABASE_URL##*/} -fSL ${DATABASE_URL}
+curl ${CREDENTIALS} -o ${INIT_DB_DIR}/${DATABASE_URL##*/} -fSL ${DATABASE_URL}
 
 docker-compose up ${CONTAINTER_NAME}
