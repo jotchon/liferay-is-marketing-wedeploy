8267af9c672a7f35169e551e038593cd8757a461
ryanschuhler <ryan.schuhler@liferay.com>
2018-07-11 15:10:25 +0200

    Upgrade container initial commit

---
+++ upgrade/scripts/update-db.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+
+CONTAINTER_DIR=/docker-entrypoint-initdb.d
+CONTAINTER_NAME=${2:-upgrade_mariadb}
+CREDENTIALS=""
+DATABASE_URL=${1:-https://files.liferay.com/private/lrdcom/www_lportal-$(date -v -1d '+%Y-%m-%d')_19-00-PDT.sql.gz}
+INIT_DB_DIR=../mariadb/docker-entrypoint-initdb.d
+REPO_ROOT=$(git rev-parse --show-toplevel)
+VOLUME_NAME=${3:-www-dxp_www_dxp_mariadb}
+
+if [ -f "${REPO_ROOT}/scripts/.username" ] && [ -f "${REPO_ROOT}/scripts/.password" ]; then
+	CREDENTIALS="-u $(cat ${REPO_ROOT}/scripts/.username):$(cat ${REPO_ROOT}/scripts/.password)"
+fi
+
+rm ${INIT_DB_DIR}/*
+curl ${CREDENTIALS} -o ${INIT_DB_DIR}/${DATABASE_URL##*/} -fSL ${DATABASE_URL}
+
+docker stop ${CONTAINTER_NAME}
+docker rm ${CONTAINTER_NAME}
+docker volume rm ${VOLUME_NAME}
+docker volume create ${VOLUME_NAME}
+
+echo -e "\n\n\n Now starting up the container to source the database. Press CTRL + C when the logs say 'MySQL init process done. Ready for start up.'\n\n\n"
+docker-compose up ${CONTAINTER_NAME}
