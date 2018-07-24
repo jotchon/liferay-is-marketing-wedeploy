25b788b354e92191194ef0ab80b5eebf056a0837
ryanschuhler <ryan.schuhler@liferay.com>
2018-07-24 08:46:04 -0700

    Keeping docker container and images in init.sh

--- www-prod/scripts/init.sh
+++ www-prod/scripts/init.sh
@@ -2,12 +2,6 @@
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
-# Delete every Docker container
-docker rm -f $(docker ps -a -q)
-
-# Delete every Docker image
-docker rmi -f $(docker images -q)
-
 docker-compose pull
 
 if [ ! -f "${REPO_ROOT}/scripts/.username" ] && [ ! -f "${REPO_ROOT}/scripts/.password" ]; then
